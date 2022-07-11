ec431f56e0d1cb697e4551ac87ce706ac0b3a8b2
Aleksandar Vukovic <vualeks@gmail.com>
2022-07-11 12:37:29 +0200

    Added SPM support

---
+++ Package.swift
@@ -0,0 +1,22 @@
+import PackageDescription
+
+let package = Package(
+    name: "Monri",
+    products: [
+        .library(
+            name: "Monri",
+            targets: ["Monri"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vualeks/Caishen.git", .upToNextMajor(from: "2.2.1")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "4.8.0"))
+    ],
+    targets: [
+        .target(
+            name: "Monri",
+            dependencies: ["Caishen", "Alamofire"],
+            path: "Monri/Classes/**/*"
+        )
+    ]
+)
