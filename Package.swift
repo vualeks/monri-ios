56034b245f88a5ee4ed2ee0eb8a91df511a4ada0
Aleksandar Vukovic <vualeks@gmail.com>
2022-07-11 12:44:58 +0200

    Target branch instead of version in SPM

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/vualeks/Caishen.git", .upToNextMajor(from: "2.2.1")),
+        .package(url: "https://github.com/vualeks/Caishen.git", .branch("master")),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "4.8.0"))
     ],
     targets: [
